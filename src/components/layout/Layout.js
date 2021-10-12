16dacee43e84af96d52c32ed93a8ff802e91b1bb
AlexandreBiguet <alexandre.biguet@easyence.com>
2021-10-12 09:32:24 +0200

    initial commit

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,12 @@
+import classes from "./Layout.module.css";
+import MainNavigation from "./MainNavigation";
+
+function Layout(props) {
+  return;
+  <div>
+    <MainNavigation />
+    <main></main>
+  </div>;
+}
+
+export default Layout;
