b8d6a3e0b6ad130a45aaf573a5358c3b320f7e5e
AlexandreBiguet <alexandre.biguet@easyence.com>
2021-10-13 17:20:47 +0200

    feat: add CORS middleware

--- back/meetup/meetup/controller.py
+++ back/meetup/meetup/controller.py
@@ -1,8 +1,8 @@
-from uuid import UUID
 import uuid
-from fastapi import FastAPI, APIRouter
-from uuid import UUID
 from typing import Dict, List
+
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 from meetup import models
 
@@ -10,17 +10,25 @@ app = FastAPI(title="meetup service", version="0.1.0")
 
 v1 = APIRouter(prefix="/v1")
 
-_meetups: Dict[UUID, models.Meetup] = {}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["POST", "GET"],
+    allow_headers=["*"],
+)
+
+_meetups: Dict[uuid.UUID, models.Meetup] = {}
 
 
 @v1.get("/meetup/{id}")
-def get_meetup(id: UUID) -> models.Meetup:
+def get_meetup(id: uuid.UUID) -> models.Meetup:
     global _meetups
     return _meetups[id]
 
 
 @v1.post("/meetup")
-def post_meetup(meetup: models.Meetup) -> UUID:
+def post_meetup(meetup: models.Meetup) -> uuid.UUID:
     uid = uuid.uuid4()
     _meetups[uid] = meetup
     return uid
