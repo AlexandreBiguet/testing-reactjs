cbb272d23896028fa4ec8279fd6db1c35dd11a43
AlexandreBiguet <alexandre.biguet@easyence.com>
2021-10-13 14:26:57 +0200

    feat: add new meetup creation page

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,11 +2,12 @@ import classes from "./Layout.module.css";
 import MainNavigation from "./MainNavigation";
 
 function Layout(props) {
-  return;
-  <div>
-    <MainNavigation />
-    <main></main>
-  </div>;
+  return (
+    <div>
+      <MainNavigation />
+      <main className={classes.main}> {props.children} </main>
+    </div>
+  );
 }
 
 export default Layout;
---
+++ src/components/meetup/NewMeetupForm.js
@@ -0,0 +1,68 @@
+import { useRef } from "react";
+
+import Card from "../ui/Card";
+
+import classes from "./NewMeetupForm.module.css";
+
+function NewMeetupForm() {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault(); // Preventing the browser to submit the form to the server
+
+    // For reading values, using Ref works OK. If we want to change the value, we probably want to use state
+    const title = titleInputRef.current.value;
+    const image = imageInputRef.current.value;
+    const address = addressInputRef.current.value;
+    const description = descriptionInputRef.current.value;
+
+    const meetup = {
+      title: title,
+      image: image,
+      address: address,
+      description: description,
+    };
+
+    console.log(meetup);
+  }
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="title"> Title </label>
+          <input type="text" required id="title" ref={titleInputRef} />
+        </div>
+
+        <div className={classes.control}>
+          <label htmlFor="image"> Image </label>
+          <input type="url" required id="image" ref={imageInputRef} />
+        </div>
+
+        <div className={classes.control}>
+          <label htmlFor="address"> Address </label>
+          <input type="text" required id="address" ref={addressInputRef} />
+        </div>
+
+        <div className={classes.control}>
+          <label htmlFor="description"> Description </label>
+          <textarea
+            required
+            id="description"
+            rows="10"
+            ref={descriptionInputRef}
+          />
+        </div>
+
+        <div className={classes.actions}>
+          <button> Add Meetup </button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;
